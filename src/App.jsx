17adcf0773d44019ea210da0ffda7dfa2de3b11e
AnushkaChaudhary1322 <anushka1322.be22@chitkara.edu.in>
2025-04-11 02:25:13 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,13 @@ function App() {
     <div className="min-h-screen animated-gradient text-gray-900">
       <Navbar />
 
-      {/* Main Layout */}
       <div className="flex py-20"> 
-        {/* Left Panel - Credential Form (Fixed Width) */}
+        {/* Left Panel */}
         <aside className="w-full md:w-[200px] lg:w-1/3 h-screen fixed top-20 left-10 px-4 overflow-y-auto ">
           <CredentialForm addCredential={addCredential} />
         </aside>
 
-        {/* Right Panel - Credential List */}
+        {/* Right Panel*/}
         <main className="ml-0 md:ml-[35%] lg:ml-[38%] w-[80%] px-10">
           <CredentialList credentials={credentials} setCredentials={setCredentials} />
         </main>
